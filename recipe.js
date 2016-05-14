2e0af0f42b2164248b1bc77d2d7b1ffd1f3a4419
seanliu93 <sean93liu@gmail.com>
2016-05-13 21:03:23 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,21 @@
 'use strict';
+function addIngredient(hash, key, val){
+  hash[key] = val;
+  return hash;
+}
+
+function removeIngredient(hash, key) {
+  delete hash[key];
+  return hash;
+}
+
+function updateIngredient(hash, key, val) {
+  hash[key] = val;
+  return hash;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] " of " key);
+  }
+}
